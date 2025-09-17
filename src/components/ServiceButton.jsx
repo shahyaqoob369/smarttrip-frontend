7b3ac90916e5b153ce84bc966d72acbf93729306
Shah <test@Tests-MacBook-Pro.local>
2025-09-17 16:36:24 +0500

    Fix: Service Buttons desing updated-21

--- src/components/ServiceButton.jsx
+++ src/components/ServiceButton.jsx
@@ -98,7 +98,7 @@ const ServiceButton = ({ service }) => {
   const buttonContent = (
   <>
     <motion.div animate={iconControls}>
-      <service.Icon className="h-24 w-20 text-white" />
+      <service.Icon className="h-28 w-24 text-white" />
     </motion.div>
     {/* 1. Margin-top changed from mt-3 to mt-1 to decrease space */}
     {/* 2. Text size changed from text-base to text-sm */}
