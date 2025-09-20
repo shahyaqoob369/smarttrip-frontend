d3062da977d53ebc0afca4f949934460f586d18f
Shah <test@Tests-MacBook-Pro.local>
2025-09-20 10:03:11 +0500

    Fix: Yacht & Crusis Widget added/setup-1

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 const MainLayout = ({ children }) => {
   return (
     // Changed bg-sky-100 to bg-blue-600 for a deep blue background
-    <div className="bg-sky-100 min-h-screen">
+    <div className="bg-brand-blue min-h-screen">
       <main>
         {children}
       </main>
