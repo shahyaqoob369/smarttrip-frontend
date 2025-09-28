63aa449a77cae287e0dbd39ca473eae05cd35113
Shah <test@Tests-MacBook-Pro.local>
2025-09-28 10:38:07 +0500

    Feat: Scroll in the languages popup-3

--- src/components/TranslateWidget.jsx
+++ src/components/TranslateWidget.jsx
@@ -2,30 +2,23 @@ import React, { useEffect } from 'react';
 
 const TranslateWidget = () => {
   useEffect(() => {
-    // Check if the script has already been added to the page
     if (document.getElementById('google-translate-script')) {
       return;
     }
-
-    // Define the initialization function and attach it to the window object
     window.googleTranslateElementInit = () => {
       new window.google.translate.TranslateElement(
         {
-          pageLanguage: 'en', // The original language of your site
+          pageLanguage: 'en',
           layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
         },
-        'google_translate_element' // The ID of the div where the widget will appear
+        'google_translate_element'
       );
     };
-
-    // Create a new script element for the Google Translate API
     const script = document.createElement('script');
     script.id = 'google-translate-script';
     script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     script.async = true;
     document.body.appendChild(script);
-
-    // This is a cleanup function that will run when the component is removed
     return () => {
       const scriptElement = document.getElementById('google-translate-script');
       if (scriptElement) {
@@ -35,8 +28,8 @@ const TranslateWidget = () => {
     };
   }, []);
 
-  // This is the div where Google will render the language switcher
-  return <div id="google_translate_element"></div>;
+  // MODIFICATION: Added 'flex-shrink-0' to prevent the widget from shrinking
+  return <div id="google_translate_element" className="flex-shrink-0"></div>;
 };
 
 export default TranslateWidget;
