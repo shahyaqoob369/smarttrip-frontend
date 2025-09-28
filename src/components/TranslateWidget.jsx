9b7809061d923b17d95ff13a89edd5066cc0e846
Shah <test@Tests-MacBook-Pro.local>
2025-09-28 10:50:59 +0500

    Feat: Scroll in the languages popup-4

--- src/components/TranslateWidget.jsx
+++ src/components/TranslateWidget.jsx
@@ -2,30 +2,31 @@ import React, { useEffect } from 'react';
 
 const TranslateWidget = () => {
   useEffect(() => {
-    // Check if the script has already been added to the page
+    // Prevent adding the script multiple times
     if (document.getElementById('google-translate-script')) {
       return;
     }
 
-    // Define the initialization function and attach it to the window object
+    // Define the initialization function
     window.googleTranslateElementInit = () => {
       new window.google.translate.TranslateElement(
         {
-          pageLanguage: 'en', // The original language of your site
+          pageLanguage: 'en', // Default language of your site
+          includedLanguages: 'en,es,fr,de,it,ru', // Optional: restrict to specific languages
           layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
         },
-        'google_translate_element' // The ID of the div where the widget will appear
+        'google_translate_element'
       );
     };
 
-    // Create a new script element for the Google Translate API
+    // Add the Google Translate script
     const script = document.createElement('script');
     script.id = 'google-translate-script';
     script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     script.async = true;
     document.body.appendChild(script);
 
-    // This is a cleanup function that will run when the component is removed
+    // Cleanup function
     return () => {
       const scriptElement = document.getElementById('google-translate-script');
       if (scriptElement) {
@@ -35,8 +36,58 @@ const TranslateWidget = () => {
     };
   }, []);
 
-  // This is the div where Google will render the language switcher
-  return <div id="google_translate_element"></div>;
+  // Widget container
+  return (
+    <div id="google_translate_element" className="my-4"></div>
+  );
 };
 
 export default TranslateWidget;
+
+
+
+
+
+
+// import React, { useEffect } from 'react';
+
+// const TranslateWidget = () => {
+//   useEffect(() => {
+//     // Check if the script has already been added to the page
+//     if (document.getElementById('google-translate-script')) {
+//       return;
+//     }
+
+//     // Define the initialization function and attach it to the window object
+//     window.googleTranslateElementInit = () => {
+//       new window.google.translate.TranslateElement(
+//         {
+//           pageLanguage: 'en', // The original language of your site
+//           layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
+//         },
+//         'google_translate_element' // The ID of the div where the widget will appear
+//       );
+//     };
+
+//     // Create a new script element for the Google Translate API
+//     const script = document.createElement('script');
+//     script.id = 'google-translate-script';
+//     script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+//     script.async = true;
+//     document.body.appendChild(script);
+
+//     // This is a cleanup function that will run when the component is removed
+//     return () => {
+//       const scriptElement = document.getElementById('google-translate-script');
+//       if (scriptElement) {
+//         document.body.removeChild(scriptElement);
+//       }
+//       delete window.googleTranslateElementInit;
+//     };
+//   }, []);
+
+//   // This is the div where Google will render the language switcher
+//   return <div id="google_translate_element"></div>;
+// };
+
+// export default TranslateWidget;
