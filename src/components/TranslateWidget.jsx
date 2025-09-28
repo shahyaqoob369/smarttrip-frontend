fc7d152d9caa9c074e9e9261cdf61e5a06803d43
Shah <test@Tests-MacBook-Pro.local>
2025-09-28 10:55:29 +0500

    Feat: Scroll in the languages popup-6

--- src/components/TranslateWidget.jsx
+++ src/components/TranslateWidget.jsx
@@ -2,12 +2,11 @@ import React, { useEffect } from 'react';
 
 const TranslateWidget = () => {
   useEffect(() => {
-    // Prevent adding the script multiple times
+    // Check if the script has already been added to the page
     if (document.getElementById('google-translate-script')) {
       return;
     }
 
-    // Define the initialization function
     // Define the initialization function and attach it to the window object
     window.googleTranslateElementInit = () => {
       new window.google.translate.TranslateElement(
@@ -19,14 +18,14 @@ const TranslateWidget = () => {
       );
     };
 
-    // Add the Google Translate script
+    // Create a new script element for the Google Translate API
     const script = document.createElement('script');
     script.id = 'google-translate-script';
     script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
     script.async = true;
     document.body.appendChild(script);
 
-    // Cleanup function
+    // This is a cleanup function that will run when the component is removed
     return () => {
       const scriptElement = document.getElementById('google-translate-script');
       if (scriptElement) {
@@ -36,13 +35,12 @@ const TranslateWidget = () => {
     };
   }, []);
 
-  // Widget container
-  return (
-    <div id="google_translate_element" className="my-4"></div>
-  );
+  // This is the div where Google will render the language switcher
+  return <div id="google_translate_element"></div>;
 };
 
 export default TranslateWidget;
+
 
 
 
@@ -53,30 +51,31 @@ export default TranslateWidget;
 
 // const TranslateWidget = () => {
 //   useEffect(() => {
-//     // Check if the script has already been added to the page
+//     // Prevent adding the script multiple times
 //     if (document.getElementById('google-translate-script')) {
 //       return;
 //     }
 
-    // // Define the initialization function and attach it to the window object
-    // window.googleTranslateElementInit = () => {
-    //   new window.google.translate.TranslateElement(
-    //     {
-    //       pageLanguage: 'en', // The original language of your site
-    //       layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
-    //     },
-    //     'google_translate_element' // The ID of the div where the widget will appear
-    //   );
-    // };
+//     // Define the initialization function
+//     window.googleTranslateElementInit = () => {
+//       new window.google.translate.TranslateElement(
+//         {
+//           pageLanguage: 'en', // Default language of your site
+//           includedLanguages: 'en,es,fr,de,it,ru', // Optional: restrict to specific languages
+//           layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
+//         },
+//         'google_translate_element'
+//       );
+//     };
 
-//     // Create a new script element for the Google Translate API
+//     // Add the Google Translate script
 //     const script = document.createElement('script');
 //     script.id = 'google-translate-script';
 //     script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
 //     script.async = true;
 //     document.body.appendChild(script);
 
-//     // This is a cleanup function that will run when the component is removed
+//     // Cleanup function
 //     return () => {
 //       const scriptElement = document.getElementById('google-translate-script');
 //       if (scriptElement) {
@@ -86,8 +85,10 @@ export default TranslateWidget;
 //     };
 //   }, []);
 
-//   // This is the div where Google will render the language switcher
-//   return <div id="google_translate_element"></div>;
+//   // Widget container
+//   return (
+//     <div id="google_translate_element" className="my-4"></div>
+//   );
 // };
 
 // export default TranslateWidget;
